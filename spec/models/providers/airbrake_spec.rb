19000dff105689b079fe1170da291bee08fe0d75
linedotstar <linedotstar@gmail.com>
2013-03-28 10:03:28 -0700

    Add pending specs for rollbar and airbrake providers

--- spec/models/providers/airbrake_spec.rb
+++ spec/models/providers/airbrake_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe WatchTower::Providers::Airbrake do
 
-  describe '#alert'
-  describe '#message'
+  describe '#alert' do
+    it 'notifies Airbrake of the exception'
+  end
+
+  describe '#message' do
+    it 'notifies Airbrake of the message'
+  end
 
 end
--- spec/models/providers/rollbar_spec.rb
+++ spec/models/providers/rollbar_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe WatchTower::Providers::Rollbar do
 
-  describe '#alert'
-  describe '#message'
+  describe '#alert' do
+    it 'reports the exception to Rollbar'
+  end
+
+  describe '#message' do
+    it 'reports the message to Rollbar'
+  end
 
 end
